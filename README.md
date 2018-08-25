7408b2652395266dc8da6c744be19e98482f4f34
Nicolas Del Valle <nicolas.delvalle@gmail.com>
2018-08-25 01:00:49 -0300

    Improve API (#35)
    * Improve API and implement prettier
    
    * Implement microbundler, improve directive and update some tests
    
    * gpg
    
    * Remove coverageThreshold (Temporal I hope)
    
    * Update deps
    
    * Update readme

--- README.md
+++ README.md
@@ -4,12 +4,10 @@
 [![Coverage Status](https://coveralls.io/repos/github/ndelvalle/v-click-outside/badge.svg?branch=master)](https://coveralls.io/github/ndelvalle/v-click-outside?branch=master)
 [![dependencies Status](https://david-dm.org/ndelvalle/v-click-outside/status.svg)](https://david-dm.org/ndelvalle/v-click-outside)
 [![devDependencies Status](https://david-dm.org/ndelvalle/v-click-outside/dev-status.svg)](https://david-dm.org/ndelvalle/v-click-outside?type=dev)
-[![Codacy Badge](https://api.codacy.com/project/badge/Grade/5ca72a2dd2044278918b45ea1eba122e)](https://www.codacy.com/app/ndelvalle/v-click-outside?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ndelvalle/v-click-outside&amp;utm_campaign=Badge_Grade)
-
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/5ca72a2dd2044278918b45ea1eba122e)](https://www.codacy.com/app/ndelvalle/v-click-outside?utm_source=github.com&utm_medium=referral&utm_content=ndelvalle/v-click-outside&utm_campaign=Badge_Grade)
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-green)](https://github.com/prettier/prettier)
 
 Vue directive to react on clicks outside an element without stopping the event propagation. Great for closing dialogues, menus among other things.
-
-
 
 ## Install
 
@@ -20,7 +18,6 @@ $ npm install --save v-click-outside
 ```bash
 $ yarn add v-click-outside
 ```
-
 
 ## Use
 
@@ -34,9 +31,26 @@ Vue.use(vClickOutside)
 ```js
 <script>
   export default {
+    data () {
+      vcoConfig: {
+        handler: this.handler,
+        middleware: this.middleware,
+        events: ['dblclick', 'click']
+      }
+    },
     methods: {
-      onClickOutside (event) {
+      onClickOutside (event, el) {
         console.log('Clicked outside. Event: ', event)
+      },
+
+      handler (event, el) {
+        console.log('Clicked outside (Using config), middleware returned true :)')
+      },
+      // Note: The middleware will be executed if the event was fired outside the element.
+      //       It should have only sync functionality and it should return a boolean to
+      //       define if the handler should be fire or not
+      middleware (event, el) {
+        return event.target.className !== 'modal'
       }
     }
   };
@@ -44,6 +58,7 @@ Vue.use(vClickOutside)
 
 <template>
   <div v-click-outside="onClickOutside"></div>
+  <div v-click-outside="vcoConfig"></div>
 </template>
 ```
 
@@ -70,16 +85,6 @@ import vClickOutside from 'v-click-outside'
 </template>
 ```
 
-On touch devices, the plugin adds 'touchstart' and 'click' event listeners to support laptops with touch screen. Use 'notouch' modifier, to avoid 'click' event to be fired.
-
-```js
-<template>
-  <div v-click-outside.notouch="onClickOutside"></div>
-</template>
-```
-
 ## License
-[MIT License](https://github.com/ndelvalle/v-click-outside/blob/master/LICENSE)
 
-## Style
-[![Standard - JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
+[MIT License](https://github.com/ndelvalle/v-click-outside/blob/master/LICENSE)
--- test/v-click-outside.test.js
+++ test/v-click-outside.test.js
@@ -1,14 +1,14 @@
-/* global jest describe it expect afterEach */
+/* global jest describe it expect beforeEach */
 
 import clickOutside from '../lib/index'
 
 const plugin = clickOutside
-const directive = clickOutside.directive
+const { directive } = clickOutside
 
 describe('v-click-outside -> plugin', () => {
   it('install the directive into the vue instance', () => {
     const vue = {
-      directive: jest.fn()
+      directive: jest.fn(),
     }
     plugin.install(vue)
     expect(vue.directive).toHaveBeenCalledWith('click-outside', directive)
@@ -17,15 +17,6 @@ describe('v-click-outside -> plugin', () => {
 })
 
 describe('v-click-outside -> directive', () => {
-  const div1 = document.createElement('div')
-  const div2 = document.createElement('div')
-  const a = document.createElement('a')
-
-  afterEach(() => {
-    directive.instances = []
-    directive.events = ['click']
-  })
-
   it('it has bind, update and unbind methods available', () => {
     expect(typeof directive.bind).toBe('function')
     expect(typeof directive.update).toBe('function')
@@ -33,108 +24,44 @@ describe('v-click-outside -> directive', () => {
   })
 
   describe('bind', () => {
-    it('adds an element to the instances list and adds an event listener (On non mobile devices)', () => {
-      directive.events = ['click']
+    beforeEach(() => {
       document.addEventListener = jest.fn()
-      directive.bind(div1, {})
-      expect(directive.instances.length).toBe(1)
-      expect(directive.instances[0].el).toBe(div1)
-      expect(document.addEventListener.mock.calls.length).toBe(1)
     })
 
-    it('adds multiple elements to the instances list and adds an event listener (On mobile devices)', () => {
-      document.addEventListener = jest.fn()
-      directive.events = ['click', 'touchstart']
-      directive.bind(div1, {})
-      directive.bind(div2, {})
-      expect(directive.instances.length).toBe(2)
-      expect(directive.instances[1].el).toBe(div2)
-      expect(document.addEventListener.mock.calls.length).toBe(2)
+    it('throws an error if the binding value is not a function or an object', () => {
+      const bind = () => directive.bind(document.createElement('div'), {})
+      expect(bind).toThrowError(/v-click-outside: Binding value must be a function or an object/)
+    })
+
+    it('adds an event listener to the element and stores an instance', () => {
+      const el = document.createElement('div')
+      const noop = () => jest.fn()
+      const config = {
+        handler: noop,
+        events: ['dblclick'],
+        middleware: noop,
+      }
+
+      directive.bind(el, { value: config })
+
+      const [instance] = directive.instances
+
+      expect(directive.instances.length).toEqual(1)
+      expect(instance.eventHandlers.length).toEqual(1)
+
+      const [eventHandler] = instance.eventHandlers
+
+      expect(typeof eventHandler.handler).toEqual('function')
+      expect(eventHandler.event).toEqual(config.events[0])
+      expect(instance.el).toBe(el)
+      expect(document.addEventListener).toHaveBeenCalledTimes(1)
     })
   })
 
   describe('update', () => {
-    it('throws an error if value is not a function', () => {
-      const updateWithNoFunction = () => directive.update(div1, {})
-      expect(updateWithNoFunction).toThrowError(/Argument must be a function/)
-    })
-
-    it('saves the callback function', () => {
-      const cb = () => {}
-      directive.bind(div1, {})
-      directive.update(div1, { value: cb })
-      expect(directive.instances[0].fn).toBe(cb)
-    })
-  })
-
-  describe('unbind', () => {
-    it('removes the instance of the list and the event listener (On non mobile devices)', () => {
-      directive.events = ['click']
-      document.removeEventListener = jest.fn()
-      directive.bind(div1, {})
-      directive.unbind(div1)
-      expect(directive.instances.length).toBe(0)
-      expect(document.removeEventListener.mock.calls.length).toBe(1)
-    })
-
-    it('removes the instance of the list and the event listener (On mobile devices)', () => {
-      directive.events = ['click', 'touchstart']
-      document.removeEventListener = jest.fn()
-      directive.bind(div1, {})
-      directive.unbind(div1)
-      expect(directive.instances.length).toBe(0)
-      expect(document.removeEventListener.mock.calls.length).toBe(2)
-    })
-
-    it('removes multiple instances of the list and the event listener', () => {
-      directive.events = ['click']
-      document.removeEventListener = jest.fn()
-      directive.bind(div1, {})
-      directive.bind(div2, {})
-      directive.unbind(div1)
-      expect(directive.instances[0].el).toBe(div2)
-      directive.unbind(div2)
-      expect(directive.instances.length).toBe(0)
-      expect(document.removeEventListener.mock.calls.length).toBe(1)
-    })
-  })
-
-  describe('onEvent', () => {
-    const messageTest1 = 'it calls the callback if the element is not the same and ' +
-                         'does not contains the event target'
-    it(messageTest1, () => {
-      const event = { target: a }
-      const cb = jest.fn()
-      directive.bind(div1, {})
-      directive.update(div1, { value: cb })
-
-      directive.onEvent(event)
-      expect(cb).toHaveBeenCalledWith(event)
-    })
-
-    const messageTest2 = 'It does not execute the callback if the event target is the ' +
-                          'element from the directive instance'
-    it(messageTest2, () => {
-      const event = { target: div1 }
-      const cb = jest.fn()
-      directive.bind(div1, {})
-      directive.update(div1, { value: cb })
-
-      directive.onEvent(event)
-      expect(cb).not.toHaveBeenCalled()
-    })
-
-    const messageTest3 = 'does not execute the callback if the event type is ' +
-                         'touchstart and the instance has notouch modifier'
-    it(messageTest3, () => {
-      const event = { target: a, type: 'touchstart' }
-      const cb = jest.fn()
-      directive.events = ['click', 'touchstart']
-      directive.bind(div1, { modifiers: { notouch: true } })
-      directive.update(div1, { value: cb })
-
-      directive.onEvent(event)
-      expect(cb).not.toHaveBeenCalled()
+    it('throws an error if the binding value is not a function or an object', () => {
+      const update = () => directive.update(document.createElement('div'), {})
+      expect(update).toThrowError(/v-click-outside: Binding value must be a function or an object/)
     })
   })
 })
