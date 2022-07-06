c316288c3619229661cce9381c686dd5c49e35bf
Renato de Leão <renatodeleao@gmail.com>
2022-07-06 19:01:17 +0100

    test: add isomorphic spec
    This is "almost" an "integration" test, to ensure the code works on actual vue environment. Add plugin instalation tests.
    
    SIDE-EFFECTS:
    - shoutout to the Dobromir for the code https://github.com/vueuse/vue-demi/issues/38#issue-802901203
    - forward correct test utils exports
    - add ifVue2 and ifVue3 test utils, when testing breaking changes — code that is not inter-compatible
    - add universalDestroy, because lifecycle hook names changed and this prevents the version check on test code

---
+++ test/test-utils.js
@@ -0,0 +1,16 @@
+const { isVue3 } = require('vue-demi')
+
+const lifecycleInstanceMethods = {
+  unmount: isVue3 ? 'unmount' : 'destroy',
+}
+
+module.exports = isVue3
+  ? require('@vue/test-utils-vue3')
+  : require('@vue/test-utils')
+
+const ifTest = (value) => (value ? it : it.skip)
+module.exports.ifVue2 = ifTest(!isVue3)
+module.exports.ifVue3 = ifTest(isVue3)
+module.exports.universalDestroy = function destroyWrapper(wrapper) {
+  if (wrapper) wrapper[lifecycleInstanceMethods.unmount]()
+}
